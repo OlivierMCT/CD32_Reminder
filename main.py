0da1ab031c452561b7a9b98d7f9f8b3e9b2f8b9e
Olivier <olivier.astre@themanis.fr>
2026-04-17 14:07:39 +0200

    first launch ! (again 2)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import sample_data
 from api.controllers.category_controller import router as category_router
 from api.controllers.todo_controller import router as todo_router
 from business.contracts.category_service import CategoryService
@@ -9,6 +10,7 @@ from business.contracts.todo_service import TodoService
 from business.services.category_service_impl import CategoryServiceImpl
 from business.services.todo_service_impl import TodoServiceImpl
 from persistence.entities.base_entity import BaseEntity
+from persistence.entities.todo_entity import TodoEntity
 from persistence.repositories.category_repository_impl import CategoryRepositoryImpl
 from persistence.repositories.todo_repository_impl import TodoRepositoryImpl
 
@@ -21,15 +23,22 @@ engine_db = create_engine('mysql://root:@localhost:3306/reminders')
 BaseEntity.metadata.create_all(engine_db)
 
 def get_todo_service() -> TodoService:
-    session = sessionmaker(autocommit=False, autoflush=False, engine_db=engine_db)()
+    session = sessionmaker(autocommit=False, autoflush=False, bind=engine_db)()
     todo_repo = TodoRepositoryImpl(session)
     cat_repo = CategoryRepositoryImpl(session)
     return TodoServiceImpl(todo_repo, cat_repo)
 
 def get_category_service() -> CategoryService:
-    session = sessionmaker(autocommit=False, autoflush=False, engine_db=engine_db)()
+    session = sessionmaker(autocommit=False, autoflush=False, bind=engine_db)()
     cat_repo = CategoryRepositoryImpl(session)
     return CategoryServiceImpl(cat_repo)
 
+def seed():
+    session = sessionmaker(autocommit=False, autoflush=False, bind=engine_db)()
+    if session.query(TodoEntity).count() == 0:
+        sample_data.add_sample_data(session)
+
 app.dependency_overrides[get_category_service] = get_category_service
 app.dependency_overrides[get_todo_service] = get_todo_service
+
+seed()
